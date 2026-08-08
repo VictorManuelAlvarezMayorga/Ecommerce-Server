00870d1af892f204ddde4c4268e1880a9da92256
VictorManuelAlvarezMayorga <utm23090718@utma.edu.mx>
2026-08-07 18:27:59 -0600

    Conexion con servidor real

--- dbcon.php
+++ dbcon.php
@@ -1,13 +1,29 @@
 <?php
+
+//Por si estamos en el servidor local (XAMPP) o en el servidor real
+$esLocal = in_array($_SERVER['SERVER_NAME'] ?? '', ['localhost', '127.0.0.1']);
+
+if ($esLocal) {
 $host = "127.0.0.1";
 $usuario = "root";
 $password = "";
 $basedatos = "ecommerce";
+} else {
+    // Configuración del Servidor de Filezila
+    $host = "datallizer.com";
+    $usuario = "datallizer_utmauser";
+    $password = "proyectoUtma2026";
+    $basedatos = "datallizer_ecommerce";
+}
 
 //conexion
 $con = new mysqli($host, $usuario, $password, $basedatos);
 
+//Conexion Falló?
 if ($con->connect_error) {
     die("Error de conexión: " . $con->connect_error);
 }
+
+//asegurar que los caracteres especiales se lean correctamente (acentos, ñ, etc.)
+mysqli_set_charset($con, "utf8mb4");
 ?>
